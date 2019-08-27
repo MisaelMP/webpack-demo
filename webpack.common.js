77f16d39a46cac787eb0ed8a76bd6b0e452d050c
Misael  M <phatarino@gmail.com>
2019-08-27 22:50:05 +1000

    added file-loader, html-loader and clean-webpack-plugin

--- webpack.common.js
+++ webpack.common.js
@@ -3,13 +3,34 @@ const HtmWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
 	entry: './src/index.js',
-	plugins: [new HtmWebpackPlugin({
-		template: './src/template.html'
-	})],
+	plugins: [
+		new HtmWebpackPlugin({
+			template: './src/template.html'
+		})
+	],
 	module: {
 		rules: [{
-			test: /\.scss$/,
-			use: ['style-loader', 'css-loader', 'sass-loader']
-		}]
+				test: /\.scss$/,
+				use: [
+					'style-loader',
+					'css-loader',
+					'sass-loader'
+				]
+			},
+			{
+				test: /\.html$/,
+				use: ['html-loader']
+			},
+			{
+				test: /\.(svg|png|jpg|gif)$/,
+				use: {
+					loader: 'file-loader',
+					options: {
+						name: '[name].[hash].[ext]',
+						outputPath: 'images'
+					}
+				}
+			}
+		]
 	}
 };
